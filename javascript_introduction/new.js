a4ca2461026f39dc689715ec9ad5f1b70cdcc650
AkankshaKurri <akankshakurri04@gmail.com>
2024-04-12 17:43:09 -0800

    Completed Javascript Introduction

---
+++ javascript_introduction/new.js
@@ -0,0 +1,4 @@
+console.log("first open terminal")
+console.log("Write node")
+console.log("Write javascript file name")
+console.log("Done..............")
---
+++ javascript_introduction/script.js
@@ -0,0 +1,22 @@
+alert("Hello World");          // page load hote hi pop hoga yeh message
+
+console.log("code is running....")                 //yeh sirf console mei likh kar aayega
+console.log("Code is looking like a wow...")
+
+var a = prompt("Enter your number:");
+var isTrue = confirm("Are you sure your number is correct and it will be saved on console");
+
+if(isTrue){
+    console.log("Saving your number")            //jab yes click karenge toh yeh likh kar aayega console mei
+}
+else{
+    console.log("Not saving your number")       //jab no click karenge toh yeh likh kar aayega console mei
+}
+
+console.log("Your number is "+a);
+
+document.title= "Hey I am good";               //javascript ki help se title change kar sakte ho..
+//document.body.style.backgroundColor = "red";
+
+//javascript frontend mei kaam aata hai
+//nodejs backend mei kaam aata hia

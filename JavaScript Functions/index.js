af2b3a0168eb374de572d02a1e51f705570d6d2a
AkankshaKurri <akankshakurri04@gmail.com>
2024-04-18 18:41:14 -0800

    Javascript Functions Completed

---
+++ JavaScript Functions/index.js
@@ -0,0 +1,40 @@
+// function nice(name){
+//     console.log("Hey " + name + " you are nice!")
+//     console.log("Hey " + name + " you are good!")
+//     console.log("Hey " + name + " your tshirt is nice")
+//     console.log("Hey " + name + " your course is good too!")
+// }
+
+// nice("Akanksha")
+// nice("Lalit")
+
+
+
+// function sum(a,b){
+//     console.log(a+b)
+// }
+
+// sum(3,5)
+
+
+// function sum(a,b,c=3){
+//     console.log(a,b,c)
+//     return a+b+c
+// }
+
+// result1=sum(3)    //gives NaN(Not a Number) because you have not passed number to parameter b
+// result2=sum(8,5)
+// result3=sum(3,9,1)
+// console.log("The sum of these numbers is:",result1)
+// console.log("The sum of these numbers is:",result2)
+// console.log("The sum of these numbers is:",result3)
+
+//arrow function
+const func1 = (x) => {
+    console.log("I am an arrow function",x);
+}
+
+func1(34);
+func1(66);
+func1(84);
+

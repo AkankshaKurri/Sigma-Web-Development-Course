186c4dd800f7c387b82e82ca4c70eac16e08b791
AkankshaKurri <akankshakurri04@gmail.com>
2024-04-19 16:25:54 -0800

    Business Name Generator Completed

---
+++ Business Name Generaor/index.js
@@ -0,0 +1,58 @@
+//Create a business name generator by combining list of adjectives and shop name and another word
+
+//Adjectives:
+//Crazy
+//Amazing
+//Fire
+
+//Shop Name:
+//Engine
+//Foods
+//Garments
+
+
+//Another Word:
+//Bros
+//Limited
+//Hub
+
+
+/*
+let adjectives=["Crazy","Amazing","Fire"]
+let shop_name=["Engine","Foods","Garments"]
+let word=["Bros","Limited", "Hub"]
+
+console.log(adjectives[0].concat(shop_name[0],word[0]))
+console.log(adjectives[0].concat(shop_name[0],word[1]))
+console.log(adjectives[0].concat(shop_name[0],word[2]))
+console.log(adjectives[0].concat(shop_name[1],word[0]))
+console.log(adjectives[0].concat(shop_name[1],word[1]))
+console.log(adjectives[0].concat(shop_name[1],word[2]))
+console.log(adjectives[0].concat(shop_name[2],word[0]))
+console.log(adjectives[0].concat(shop_name[2],word[1]))
+console.log(adjectives[0].concat(shop_name[2],word[2]))
+
+console.log(adjectives[1].concat(shop_name[0],word[0]))
+console.log(adjectives[1].concat(shop_name[0],word[1]))
+console.log(adjectives[1].concat(shop_name[0],word[2]))
+console.log(adjectives[1].concat(shop_name[1],word[0]))
+console.log(adjectives[1].concat(shop_name[1],word[1]))
+console.log(adjectives[1].concat(shop_name[1],word[2]))
+console.log(adjectives[1].concat(shop_name[2],word[0]))
+console.log(adjectives[1].concat(shop_name[2],word[1]))
+console.log(adjectives[1].concat(shop_name[2],word[2]))
+
+console.log(adjectives[2].concat(shop_name[0],word[0]))
+console.log(adjectives[2].concat(shop_name[0],word[1]))
+console.log(adjectives[2].concat(shop_name[0],word[2]))
+console.log(adjectives[2].concat(shop_name[1],word[0]))
+console.log(adjectives[2].concat(shop_name[1],word[1]))
+console.log(adjectives[2].concat(shop_name[1],word[2]))
+console.log(adjectives[2].concat(shop_name[2],word[0]))
+console.log(adjectives[2].concat(shop_name[2],word[1]))
+console.log(adjectives[2].concat(shop_name[2],word[2]))
+
+*/
+
+
+
